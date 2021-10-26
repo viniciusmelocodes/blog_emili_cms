7cf483b10025489ebc63cbaaeedd6c7f46c964ab
Vinicius Melo <viniciusldemelo@gmail.com>
2021-10-26 01:30:28 -0300

    Correções

--- resources/views/blog/aprenda-com-a-emili/aprenda-com-a-emili.blade.php
+++ resources/views/blog/aprenda-com-a-emili/aprenda-com-a-emili.blade.php
@@ -5,7 +5,7 @@
 <div class="row conteudo-blog" style="padding-top: 30px; padding-bottom: 30px">
     <div class="col-3"></div>
     <div class="col-6">
-        <iframe class="video-emili" height="800px" width="800px" src="https://www.youtube.com/embed/PULsV_vLGV8"
+        <iframe class="video-emili" height="100%" width="100%" src="https://www.youtube.com/embed/PULsV_vLGV8"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen>
